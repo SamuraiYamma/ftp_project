73fbae393339a883fee966fb088183cad339ef5c
Ethan Carter <acrealabamawoo@gmail.com>
2019-09-27 15:32:15 -0400

    Add files via upload

--- src/ftp_client.java
+++ src/ftp_client.java
@@ -11,13 +11,18 @@ public class ftp_client {
         try {
             //scanner obj
             Scanner scn = new Scanner(System.in);
+            InetAddress ip = null;
+            int port = 0;
+            if(scn.next().equalsIgnoreCase("CONNECT")) {
+                //get IP
+                ip = convertToIP(scn.next());
+                //get Port
+                port = validatePort(scn.next());
 
-
-            //get IP
-            InetAddress ip = convertToIP(args[0]);
-            //get Port
-            int port = validatePort(args[1]);
-
+            }
+            else{
+                System.exit(-1);
+            }
             //establish new socket connection
             Socket clientsocket = new Socket(ip, port);
 
@@ -44,30 +49,34 @@ public class ftp_client {
                     switch (tosend) {
                         case "LIST":
                             listFiles();
+                            break;
                         case "RETRIEVE":
                             recieveFile();
+                            break;
                         case "STORE":
-                            sendFile(scn.next());
+                            sendFile(scn.next(), dis, dos, scn);
+                            break;
                         case "HELP":
-                            printHELP();
+                        case "?":
+                        case "h":
+                        case "-h":
                         case "help":
                             printHELP();
-                        case "-h":
-                            printHELP();
-                        case "h":
-                            printHELP();
-                        case "?":
-                            printHELP();
+                            break;
                         default:
                             System.out.printf("Command: '%s' not recognized\n", tosend);
+                            break;
                     }
                 }
                 catch (FileNotFoundException e){
-                e.printStackTrace();
-                System.out.printf("File not found\n");
+                    e.printStackTrace();
+                    System.out.printf("File not found\n");
+                } catch (IOException e){
+                    e.printStackTrace();
+                    System.out.printf("Could not get response from server");
                 } catch (Exception e){
-                e.printStackTrace();
-                System.out.printf("Not sure what went wrong\n");
+                    e.printStackTrace();
+                    System.out.printf("Not sure what went wrong\n");
                 }
             }
 
@@ -83,6 +92,9 @@ public class ftp_client {
         } catch (FileNotFoundException e){
             e.printStackTrace();
             System.out.printf("File not found\n");
+        } catch (SocketException e) {
+            e.printStackTrace();
+            System.out.printf("Connect reset by host\n");
         } catch (IOException e) {
             e.printStackTrace();
             System.out.printf("Could not establish socket: %s:%s\n", args[0], args[1]);
@@ -90,28 +102,63 @@ public class ftp_client {
             e.printStackTrace();
             System.out.printf("Not sure what went wrong\n");
         } finally {
-            System.out.println("Terminating process...");
+            System.out.println("Terminating process...\n");
             System.exit(-1);
         }
 
     }
     //METHODS
     static void printHELP(){
-        System.out.printf("HELP SECTION --- CLIENT COMMANDS\n" +
-                "CONNECT <address> <port>\n" +
+        System.out.printf("\n--------------------------------------" +
+                "\nHELP SECTION --- CLIENT COMMANDS\n" +
                 "LIST\n" +
                 "RETRIEVE <filename>\n" +
                 "STORE <filename>\n" +
-                "QUIT\n");
+                "QUIT\n" +
+                "--------------------------------------\n");
     }
     static void listFiles(){
     }
     static void recieveFile(){
     }
-    static void sendFile(String filename) throws FileNotFoundException {
+    static void sendFile(String filename, DataInputStream dis, DataOutputStream dos, Scanner scn) throws IOException {
         File file = new File(filename);
         if(!file.exists()){
             throw new FileNotFoundException();
+        }
+        //get input from server
+        String response = dis.readUTF();
+        if(response.equalsIgnoreCase("File already exists")){
+            String choice;
+            System.out.print("File already exists. Would you like to overwrite it? (Y/N)");
+            choice = scn.next();
+            //TODO: We should validate that it is a string
+            switch (choice){
+                case "Y":
+                    //cool
+                case "y":
+                    //cool
+                case "N":
+                case "n":
+                    dos.flush();
+                    return;
+                default:
+                    System.out.print("Could not understand. Please enter Y or N.");
+            }
+            //get ready to send file
+            FileOutputStream fos = new FileOutputStream(file);
+            int ch;
+            String tmp;
+            do{
+                tmp = dis.readUTF();
+                ch = Integer.parseInt(tmp);
+                if(ch != -1){
+                    fos.write(ch);
+                }
+            }
+            while(ch != -1);
+            fos.close();
+            //maybe show prompt conveying transfer success?
         }
     }
 
--- src/ftp_server.java
+++ src/ftp_server.java
@@ -10,7 +10,49 @@ public class ftp_server {
         }
 
         int port = validatePort(args[0]);
+        Scanner scn = new Scanner(System.in);
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
 
+//            //ACCEPT THE QUIT COMMAND
+//            if(scn.nextLine().equalsIgnoreCase("QUIT")){
+//                System.out.print("Force closing the server...\n");
+//                System.exit(0);
+//            }
+
+            while (true)
+            {
+                Socket s = null;
+                try
+                {
+                    // socket object to receive incoming client requests
+                    s = serverSocket.accept();
+
+                    System.out.println("A new client is connected : " + s);
+
+                    // obtaining input and out streams
+                    DataInputStream dis = new DataInputStream(s.getInputStream());
+                    DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+                    System.out.println("Assigning new thread for this client");
+
+                    // create a new thread object
+                    Thread t = new ClientHandler(s, dis, dos);
+
+                    // Invoking the start() method
+                    t.start();
+                }
+                catch (Exception e){
+                    s.close();
+                    e.printStackTrace();
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Could not open server");
+        }
 
     }
 
@@ -32,5 +74,87 @@ public class ftp_server {
             e.printStackTrace();
         }
         return Integer.parseInt(null);
+    }
+}
+
+// ClientHandler class
+class ClientHandler extends Thread
+{
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    final Socket s;
+
+
+    // Constructor
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos)
+    {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+    }
+
+    @Override
+    public void run()
+    {
+        String received;
+        String toreturn;
+        while (true)
+        {
+            try {
+
+                // Ask user what he wants
+                dos.writeUTF("Enter command or enter HELP to view commands: \n");
+
+                // receive the answer from client
+                received = dis.readUTF();
+
+                if(received.equalsIgnoreCase("QUIT"))
+                {
+                    System.out.println("Client " + this.s + " sends exit...");
+                    System.out.println("Closing this connection.");
+                    this.s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+
+                /**
+                 * Expected output streams are:
+                 *  - File already exists
+                 *  -
+                 */
+                // write on output stream based on the
+                // answer from the client
+                else if(received.substring(0,3).equalsIgnoreCase("LIST")){
+                    //TODO create list function
+                    dos.writeUTF("listing files...\n");
+                }
+                else if (received.substring(0,4).equalsIgnoreCase("STORE")){
+                    //TODO create store function
+                    String fileName = received.substring(5);
+                    dos.writeUTF("storing " + fileName + "...");
+                }
+                else if (received.substring(0, 6).equalsIgnoreCase("RETRIEVE")){
+                    //TODO create retrieve function
+                    String fileName = received.substring(7);
+                    dos.writeUTF("retrieving " + fileName + "...");
+                }
+                else{
+                    dos.writeUTF("Invalid Input");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try
+        {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
